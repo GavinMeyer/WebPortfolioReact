81ec7c8204c23a8fdfdef2e88cef7615044e6646
GavinMeyer <gavinplay@live.com>
2024-09-27 17:14:56 -0400

    v0.1.5
    some entrance page progress

--- src/Entrance.js
+++ src/Entrance.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Entrance(){
     return(
-        <div>
-            <p>entrance</p>
-            <br />
-            <Link to="/home" >LEARN ABOUT ME</Link>
+        <div className="mainDiv">
+            <div className="innerDiv">
+                <h1>WELCOME TO GAVIN MEYER'S PORTFOLIO</h1>
+                <br />
+                <Link to="/home" >LEARN ABOUT ME</Link>
+            </div>
         </div>
     );
 }
